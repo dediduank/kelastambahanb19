3f15a24d2a11933dd031148d979bbc26b09e8808
Wardani <dediduank@gmail.com>
2022-01-10 14:35:30 +0700

    create controller

--- src/main/java/extra/classb/accessdb/myController.java
+++ src/main/java/extra/classb/accessdb/myController.java
@@ -4,10 +4,36 @@
  */
 package extra.classb.accessdb;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  *
  * @author Dedi Wardani
  */
+@CrossOrigin
+@RestController
 public class myController {
+
+    MahasiswaJpaController mhs = new MahasiswaJpaController();
     
+    @RequestMapping("/showdata")
+    @ResponseBody
+    public List<Mahasiswa> getData(){
+        
+         List<Mahasiswa> mhsb = new ArrayList<>();    
+        try {
+            
+            mhsb = mhs.findMahasiswaEntities();
+            
+        } catch (Exception e) {
+        }
+        
+        return mhsb;
+        
+    }
 }
